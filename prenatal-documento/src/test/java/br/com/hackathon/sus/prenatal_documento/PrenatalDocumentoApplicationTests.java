947eba3ba3cc950784e96694ee9b5fcf457dbe38
luizffdemoraes <lffm1994@gmail.com>
2026-01-30 02:18:25 -0300

    Fix test prenatal-documento

--- prenatal-documento/src/test/java/br/com/hackathon/sus/prenatal_documento/PrenatalDocumentoApplicationTests.java
+++ prenatal-documento/src/test/java/br/com/hackathon/sus/prenatal_documento/PrenatalDocumentoApplicationTests.java
@@ -3,8 +3,10 @@ package br.com.hackathon.sus.prenatal_documento;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PrenatalDocumentoApplicationTests {
 
 	@Test
